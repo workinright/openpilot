bfadd896576545eaf084ad6a4408109c38205ede
workinright <workinright@proton.me>
2025-07-17 07:05:25 +0200

    update

--- selfdrive/test/docker_build.sh
+++ selfdrive/test/docker_build.sh
@@ -17,12 +17,12 @@ fi
 
 source $SCRIPT_DIR/docker_common.sh $1 "$TAG_SUFFIX"
 
-sudo apt-get -y install strace
+#sudo apt-get -y install strace
 
-DOCKER_BUILDKIT=1 strace -f docker buildx build --output type=image,compression=zstd --provenance false --pull --platform $PLATFORM --load --cache-to type=inline --cache-from type=registry,ref=$REMOTE_TAG -t $DOCKER_IMAGE:latest -t $REMOTE_TAG -t $LOCAL_TAG -f $OPENPILOT_DIR/$DOCKER_FILE $OPENPILOT_DIR
+DOCKER_BUILDKIT=1 docker buildx build --output type=image,compression=zstd --provenance false --pull --platform $PLATFORM --load --build-arg BUILDKIT_INLINE_CACHE=1 --cache-to type=registry,ref=$REMOTE_TAG,type=inline --cache-from type=registry,ref=$REMOTE_TAG -t $DOCKER_IMAGE:latest -t $REMOTE_TAG -t $LOCAL_TAG -f $OPENPILOT_DIR/$DOCKER_FILE $OPENPILOT_DIR
 
-#if [ -n "$PUSH_IMAGE" ]; then
-#  docker push $REMOTE_TAG
-#  docker tag $REMOTE_TAG $REMOTE_SHA_TAG
-#  docker push $REMOTE_SHA_TAG
-#fi
+if [ -n "$PUSH_IMAGE" ]; then
+  docker push $REMOTE_TAG
+  docker tag $REMOTE_TAG $REMOTE_SHA_TAG
+  docker push $REMOTE_SHA_TAG
+fi
