40cadeca2d467d0fe85cbabc3b1d1846f63146ab
workinright <workinright@proton.me>
2025-07-17 06:51:42 +0200

    update

--- selfdrive/test/docker_build.sh
+++ selfdrive/test/docker_build.sh
@@ -17,7 +17,7 @@ fi
 
 source $SCRIPT_DIR/docker_common.sh $1 "$TAG_SUFFIX"
 
-apt-get -y install strace
+sudo apt-get -y install strace
 
 DOCKER_BUILDKIT=1 strace -f docker buildx build --output type=image,compression=zstd --provenance false --pull --platform $PLATFORM --load --cache-to type=inline --cache-from type=registry,ref=$REMOTE_TAG -t $DOCKER_IMAGE:latest -t $REMOTE_TAG -t $LOCAL_TAG -f $OPENPILOT_DIR/$DOCKER_FILE $OPENPILOT_DIR
 
