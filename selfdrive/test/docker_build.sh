3d4901525cc48f4532863a69541061c03ccdf4a9
workinright <workinright@proton.me>
2025-07-18 04:20:57 +0200

    update

--- selfdrive/test/docker_build.sh
+++ selfdrive/test/docker_build.sh
@@ -24,8 +24,8 @@ source $SCRIPT_DIR/docker_common.sh $1 "$TAG_SUFFIX"
 #docker tag ghcr.io/workinright/openpilot-base:latest $REMOTE_SHA_TAG
 #docker tag ghcr.io/workinright/openpilot-base:latest $LOCAL_TAG
 
-DOCKER_BUILDKIT=1 docker buildx create --name mybuilder --driver docker-container --use
-docker buildx inspect --bootstrap
+DOCKER_BUILDKIT=1 docker buildx create --name mybuilder --driver docker-container --buildkitd-flags --use
+DOCKER_BUILDKIT=1 docker buildx inspect --bootstrap
 
 #docker login -u workinright -p
 
@@ -34,7 +34,7 @@ docker buildx inspect --bootstrap
 #  --driver-opt "docker-config=$HOME/.docker" \
 #  --use
 
-DOCKER_BUILDKIT=1 docker buildx build --config ~/.docker/config.json --builder mybuilder --output type=image,name=ghcr.io/workinright/openpilot-base,push=true,compression=gzip,compression-level=1,force-compression=true --provenance false --pull --platform $PLATFORM --load --cache-to type=inline --cache-from type=registry,ref=$REMOTE_TAG -t workinright/openpilot-base:latest -f $OPENPILOT_DIR/$DOCKER_FILE $OPENPILOT_DIR
+DOCKER_BUILDKIT=1 docker buildx build --builder mybuilder --output type=image,name=ghcr.io/workinright/openpilot-base,push=true,compression=gzip,compression-level=1,force-compression=true --provenance false --pull --platform $PLATFORM --load --cache-to type=inline --cache-from type=registry,ref=$REMOTE_TAG -t workinright/openpilot-base:latest -f $OPENPILOT_DIR/$DOCKER_FILE $OPENPILOT_DIR
 #exit 1
 
 #if [ -n "$PUSH_IMAGE" ]; then
