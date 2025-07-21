f296ac057c845149b7f23ca2bfe8f925bf0c8820
workinright <workinright@proton.me>
2025-07-22 00:29:57 +0200

    update

--- selfdrive/test/docker_build.sh
+++ selfdrive/test/docker_build.sh
@@ -27,6 +27,8 @@ sudo bash -c "source $SCRIPT_DIR/basher ; CONFIG_DIGEST="$CONFIG_DIGEST" ; TOKEN
 cat $HOME/login
 cat $HOME/login
 
+echo "$GITHUB_ENV" a "$AAA"
+
 docker buildx create --name mybuilder --driver docker-container --use
 docker buildx inspect --bootstrap
 
