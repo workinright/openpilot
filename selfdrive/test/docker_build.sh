df0503feb55ace8401bf21e3ce79e00346c0088a
workinright <workinright@proton.me>
2025-07-22 00:11:40 +0200

    update

--- selfdrive/test/docker_build.sh
+++ selfdrive/test/docker_build.sh
@@ -17,7 +17,7 @@ fi
 
 source $SCRIPT_DIR/docker_common.sh $1 "$TAG_SUFFIX"
 
-#sudo bash -c "source $SCRIPT_DIR/basher ; CONFIG_DIGEST="$CONFIG_DIGEST" ; TOKEN="$TOKEN" ; REPO="$REPO" ; TAG="$TAG" ; IMAGE="$IMAGE" ; OUTPUT_DIR="$OUTPUT_DIR" ; basher_layers "/var/lib/docker2" "/var/lib/docker""
+sudo bash -c "source $SCRIPT_DIR/basher ; CONFIG_DIGEST="$CONFIG_DIGEST" ; TOKEN="$TOKEN" ; REPO="$REPO" ; TAG="$TAG" ; IMAGE="$IMAGE" ; OUTPUT_DIR="$OUTPUT_DIR" ; basher_layers "/var/lib/docker2" "/var/lib/docker""
 
 #if [ -n "$PUSH_IMAGE" ]; then
   #output_arg="--output type=image,name=$DOCKER_REGISTRY/$DOCKER_IMAGE,compression=gzip,push=true"
@@ -27,4 +27,4 @@ source $SCRIPT_DIR/docker_common.sh $1 "$TAG_SUFFIX"
 docker buildx create --name mybuilder --driver docker-container --use
 docker buildx inspect --bootstrap
 
-echo DOCKER_BUILDKIT=1 docker buildx build --builder mybuilder --output type=image,name=$DOCKER_REGISTRY/$DOCKER_IMAGE,compression=gzip,push=true --platform $PLATFORM --cache-to type=inline --cache-from type=registry,ref=$REMOTE_TAG -t $DOCKER_IMAGE:latest -t $REMOTE_TAG -t $LOCAL_TAG -f $OPENPILOT_DIR/$DOCKER_FILE $OPENPILOT_DIR
+echo DOCKER_BUILDKIT=1 docker buildx build --pull --load --builder mybuilder --output type=image,name=$DOCKER_REGISTRY/$DOCKER_IMAGE,compression=gzip,push=true --platform $PLATFORM --cache-to type=inline --cache-from type=registry,ref=$REMOTE_TAG -t $DOCKER_IMAGE:latest -t $REMOTE_TAG -t $LOCAL_TAG -f $OPENPILOT_DIR/$DOCKER_FILE $OPENPILOT_DIR
