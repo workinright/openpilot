116e00e228991cfa065e908df3b9b107e28d92eb
workinright <workinright@proton.me>
2025-07-22 04:00:09 +0200

    update

--- selfdrive/test/docker_build.sh
+++ selfdrive/test/docker_build.sh
@@ -28,9 +28,9 @@ cat $HOME/login
 cat $HOME/login
 
 #echo "$GITHUB_ENV" a "$AAA"
-docker login ghcr.io "$AAA"
+DOCKER_BUILDKIT=1 docker login ghcr.io $AAA
 
-docker buildx create --name mybuilder --driver docker-container --use
-docker buildx inspect --bootstrap
+DOCKER_BUILDKIT=1 docker buildx create --name mybuilder --driver docker-container --use
+DOCKER_BUILDKIT=1 docker buildx inspect --bootstrap
 
 DOCKER_BUILDKIT=1 docker buildx build --pull --load --push --builder mybuilder --output type=image,name=ghcr.io/workinright/openpilot-base,compression=gzip,push=true --platform $PLATFORM --cache-to type=inline --cache-from type=registry,ref=$REMOTE_TAG -t $DOCKER_IMAGE:latest -f $OPENPILOT_DIR/$DOCKER_FILE $OPENPILOT_DIR
