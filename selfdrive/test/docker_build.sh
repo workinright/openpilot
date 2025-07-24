ecd5e77b16e7916049b6e9ed57e06a8004e95590
workinright <workinright@proton.me>
2025-07-24 08:20:04 +0200

    update

--- selfdrive/test/docker_build.sh
+++ selfdrive/test/docker_build.sh
@@ -20,9 +20,7 @@ source $SCRIPT_DIR/docker_common.sh $1 "$TAG_SUFFIX"
 
 sudo bash -c "source $SCRIPT_DIR/basher ; CONFIG_DIGEST="$CONFIG_DIGEST" ; TOKEN="$TOKEN" ; REPO="$REPO" ; TAG="$TAG" ; IMAGE="$IMAGE" ; OUTPUT_DIR="$OUTPUT_DIR" ; basher_layers "/var/lib/docker2" "/var/lib/docker"" || true
 
-echo docker
-docker images --format "{{.ID}}"
-echo dockerend
+sha256_10="$(docker images --no-trunc --format "{{.ID}}")"
 
 echo AAA $AAA "$(cat "$HOME/github_credentials")"
 if [ ! -e "$HOME/github_credentials" ] && [ ! -z "$AAA" ]
@@ -33,8 +31,14 @@ echo AAB $AAA "$(cat "$HOME/github_credentials")"
 DOCKER_BUILDKIT=1 docker login ghcr.io $(cat "$HOME/github_credentials")
 
 output="$(DOCKER_BUILDKIT=1 docker buildx build --progress=plain --load --platform $PLATFORM --cache-to type=inline --cache-from type=registry,ref=$REMOTE_TAG -t ghcr.io/workinright/openpilot-base -f $OPENPILOT_DIR/$DOCKER_FILE $OPENPILOT_DIR 2>&1)"
-echo "$output" | grep sha256
+sha256_11="$(echo "$output" | tail -n1 | grep sha256)"
 
-if [ -n "$PUSH_IMAGE" ]; then
+echo sha
+echo "$sha256_10"
+echo "$sha256_11"
+echo shaend
+
+if [ -n "$PUSH_IMAGE" ] && [ "$sha256_10" != "$sha256_11" ]
+then
   docker push ghcr.io/workinright/openpilot-base
 fi
