646f4c0fe87ca20c217e030bc750edae3671daab
workinright <workinright@proton.me>
2025-07-24 22:35:00 +0200

    update

--- selfdrive/test/docker_build.sh
+++ selfdrive/test/docker_build.sh
@@ -60,8 +60,10 @@ sha256_11="$(echo "$output" | grep sha256 | tail -n1 | cut -d':' -f2 | cut -d' '
     DOCKER_BUILDKIT=1 docker buildx create --name mybuilder --driver docker-container --use
   DOCKER_BUILDKIT=1 docker buildx inspect --bootstrap
     
-    output2="$(DOCKER_BUILDKIT=1 docker buildx build $flags --progress=plain --load --builder mybuilder --platform $PLATFORM --cache-to type=inline --cache-from type=registry,ref=$REMOTE_TAG -t ghcr.io/workinright/openpilot-base -f $OPENPILOT_DIR/$DOCKER_FILE $OPENPILOT_DIR 2>&1)"
-    echo output2 $output2
+    #output2="$(
+    DOCKER_BUILDKIT=1 docker buildx build $flags --progress=plain --load --builder mybuilder --platform $PLATFORM --cache-to type=inline --cache-from type=registry,ref=$REMOTE_TAG -t ghcr.io/workinright/openpilot-base -f $OPENPILOT_DIR/$DOCKER_FILE $OPENPILOT_DIR
+    #2>&1)"
+    #echo output2 $output2
 
     stat myimage || true
     find myimage
