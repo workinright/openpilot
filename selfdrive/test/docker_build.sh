8340621fb20285ac5d2f288cc1e320bf4afa1c5b
workinright <workinright@proton.me>
2025-07-24 08:25:57 +0200

    update

--- selfdrive/test/docker_build.sh
+++ selfdrive/test/docker_build.sh
@@ -31,7 +31,7 @@ echo AAB $AAA "$(cat "$HOME/github_credentials")"
 DOCKER_BUILDKIT=1 docker login ghcr.io $(cat "$HOME/github_credentials")
 
 output="$(DOCKER_BUILDKIT=1 docker buildx build --progress=plain --load --platform $PLATFORM --cache-to type=inline --cache-from type=registry,ref=$REMOTE_TAG -t ghcr.io/workinright/openpilot-base -f $OPENPILOT_DIR/$DOCKER_FILE $OPENPILOT_DIR 2>&1)"
-echo output
+echo output $output
 sha256_11="$(echo "$output" | tail -n1 | grep sha256)" || true
 
 echo sha1
