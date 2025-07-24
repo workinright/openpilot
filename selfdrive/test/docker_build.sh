05aa75d02a1c6fde181e683836b0cc4b0d224a75
workinright <workinright@proton.me>
2025-07-24 08:09:15 +0200

    update

--- selfdrive/test/docker_build.sh
+++ selfdrive/test/docker_build.sh
@@ -30,7 +30,7 @@ fi
 echo AAB $AAA "$(cat "$HOME/github_credentials")"
 DOCKER_BUILDKIT=1 docker login ghcr.io $(cat "$HOME/github_credentials")
 
-output="$(DOCKER_BUILDKIT=1 docker buildx build --progress=plain --load --platform $PLATFORM --cache-to type=inline --cache-from type=registry,ref=$REMOTE_TAG -t ghcr.io/workinright/openpilot-base -f $OPENPILOT_DIR/$DOCKER_FILE $OPENPILOT_DIR)"
+output="$(DOCKER_BUILDKIT=1 docker buildx build --progress=plain --load --platform $PLATFORM --cache-to type=inline --cache-from type=registry,ref=$REMOTE_TAG -t ghcr.io/workinright/openpilot-base -f $OPENPILOT_DIR/$DOCKER_FILE $OPENPILOT_DIR 2>&1)"
 echo "$output" | grep sha256
 
 if [ -n "$PUSH_IMAGE" ]; then
