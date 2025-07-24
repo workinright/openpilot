64dbbe2d4855a5384cf41e85295c135f7b9e6052
workinright <workinright@proton.me>
2025-07-24 21:01:58 +0200

    update

--- selfdrive/test/docker_build.sh
+++ selfdrive/test/docker_build.sh
@@ -41,7 +41,6 @@ date
 output="$(DOCKER_BUILDKIT=1 docker buildx build --progress=plain --load --platform $PLATFORM --cache-to type=inline --cache-from type=registry,ref=$REMOTE_TAG -t ghcr.io/workinright/openpilot-base -f $OPENPILOT_DIR/$DOCKER_FILE $OPENPILOT_DIR 2>&1)"
 date
 echo output $output
-stat myimage.tar || true
 #echo output $output
 sha256_11="$(echo "$output" | grep sha256 | tail -n1 | cut -d':' -f2 | cut -d' ' -f1)" || true
 
@@ -51,9 +50,9 @@ sha256_11="$(echo "$output" | grep sha256 | tail -n1 | cut -d':' -f2 | cut -d' '
 #echo "$sha256_11"
 #echo shaend
 
-if [ -n "$PUSH_IMAGE" ] && [ "$sha256_10" != "$sha256_11" ]
-then
-  if [ "$use_zstd" = 1 ]
+##if [ -n "$PUSH_IMAGE" ] && [ "$sha256_10" != "$sha256_11" ]
+##then
+  ##if [ "$use_zstd" = 1 ]
   then
     # Zstandard uploading is broken in docker buildx!
 
@@ -63,8 +62,10 @@ then
     output2="$(DOCKER_BUILDKIT=1 docker buildx build $flags --progress=plain --load --builder mybuilder --platform $PLATFORM --cache-to type=inline --cache-from type=registry,ref=$REMOTE_TAG -t ghcr.io/workinright/openpilot-base -f $OPENPILOT_DIR/$DOCKER_FILE $OPENPILOT_DIR 2>&1)"
     echo output2 $output2
 
+    stat myimage.tar || true
+
   fi
 
   DOCKER_BUILDKIT=1 docker login ghcr.io $(cat "$HOME/github_credentials")
   docker push ghcr.io/workinright/openpilot-base
-fi
+##fi
