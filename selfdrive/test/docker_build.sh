9ca668dcab32c62928841513f1062644f803f190
workinright <workinright@proton.me>
2025-07-24 08:39:48 +0200

    update

--- selfdrive/test/docker_build.sh
+++ selfdrive/test/docker_build.sh
@@ -22,15 +22,17 @@ sudo bash -c "source $SCRIPT_DIR/basher ; CONFIG_DIGEST="$CONFIG_DIGEST" ; TOKEN
 
 sha256_10="$(docker images --no-trunc --format "{{.ID}}" | cut -d':' -f2 | cut -d' ' -f1)"
 
-echo AAA $AAA "$(cat "$HOME/github_credentials")"
+#echo AAA $AAA "$(cat "$HOME/github_credentials")"
 if [ ! -e "$HOME/github_credentials" ] && [ ! -z "$AAA" ]
 then
   echo "$AAA" > "$HOME/github_credentials"
 fi
-echo AAB $AAA "$(cat "$HOME/github_credentials")"
+#echo AAB $AAA "$(cat "$HOME/github_credentials")"
 
+date
 output="$(DOCKER_BUILDKIT=1 docker buildx build --progress=plain --load --platform $PLATFORM --cache-to type=inline --cache-from type=registry,ref=$REMOTE_TAG -t ghcr.io/workinright/openpilot-base -f $OPENPILOT_DIR/$DOCKER_FILE $OPENPILOT_DIR 2>&1)"
-echo output $output
+date
+#echo output $output
 sha256_11="$(echo "$output" | grep sha256 | tail -n1 | cut -d':' -f2 | cut -d' ' -f1)" || true
 
 echo sha1
